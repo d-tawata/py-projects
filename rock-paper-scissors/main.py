f47e65849be7e7f00069cb04e5276c1c97afd003
Diana Tawata <diana@tawata.me>
2024-05-02 16:39:35 -0700

    updated game with functions

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,39 +1,44 @@
 import random
-
-def reset_score():
-       score['user'] = 0
-       score['computer'] = 0
-
-lose_message = 'You lost!'
-win_message = 'You won!'
-tie_message = 'Tie!'
-
-choices = ['rock', 'paper', 'scissors']
-winner_matrix = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
-
-user_input = ''
-game_over = False
-score = {'user': 0, 'computer': 0}
-while not game_over:
-       user_input = input('Type rock, paper, or scissors: ')
-       computer_input = random.choice(choices)
-       if user_input == computer_input:
-              message = tie_message
-       elif winner_matrix[user_input] == computer_input:
-              message = win_message
-              score['user'] += 1
-       else:
-              message = lose_message 
-              score['computer'] += 1
-       print(f'{message} You chose {user_input} and the computer chose {computer_input}. The current score is: You: {score["user"]} Computer: {score["computer"]}')
        
-       # print result and restart game if user wants to play again
-       if (score['user'] == 3 | score['computer'] == 3):
-           if score['user'] == 3:
-                  final_message = win_message
-           else:
-                  final_message = lose_message
-           print(f'Game over!. {final_message} The final score is: You: {score["user"]} Computer: {score["computer"]}')
-           game_over = input('Play again? (y/n) ') == 'n'
-           reset_score()
-           
+def check_game_end(score):
+        game_end = score['user'] == 3 | score['computer'] == 3
+        if (game_end):
+            win_status = 'won!' if score['user'] == 3 else 'lost!'
+            print(f'Game over!. You {win_status} The final score is: You: {score["user"]} Computer: {score["computer"]}')
+        return game_end
+        
+def play_round(user_input, computer_input, score):
+        lose_message = 'You lost!'
+        win_message = 'You won!'
+        tie_message = 'Tie!'
+        
+        winner_matrix = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+    
+        if user_input == computer_input:
+                message = tie_message
+        elif winner_matrix[user_input] == computer_input:
+                message = win_message
+                score['user'] += 1
+        else:
+                message = lose_message 
+                score['computer'] += 1
+        print(f'{message} You chose {user_input} and the computer chose {computer_input}. The current score is: You: {score["user"]} Computer: {score["computer"]}')
+            
+def main():
+    choices = ['rock', 'paper', 'scissors']
+    score = {'user': 0, 'computer': 0}
+    
+    while True:
+        user_input = input('Type rock, paper, or scissors: ').lower()
+        while user_input not in choices:
+            user_input = input('Invalid input. Type rock, paper, or scissors: ').lower()
+        computer_input = random.choice(choices)
+        play_round(user_input, computer_input, score)
+        
+        if check_game_end(score):
+            if input('Do you want to play again? (y/n) ') == 'n':
+                break
+            score = {'user': 0, 'computer': 0}
+    
+if __name__ == '__main__':
+    main()
